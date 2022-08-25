7f35da05afe55a7fc6d92752546f3321bfc2fc93
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-08-25 16:53:46 -0400

    Adding Pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,11 +1,27 @@
 const { validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
 
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res.status(200).json({ message: "Fetched all Posts!", posts: posts });
+      res.status(200).json({
+        message: "Fetched all Posts!",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -23,7 +39,7 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
   if (!req.file) {
-    const error = new Error("No Image Providaded!");
+    const error = new Error("No Image Provided!");
     error.statusCode = 422;
     throw error;
   }
@@ -72,4 +88,86 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+
+  if (!errors.isEmpty()) {
+    const error = new Error("Entered Data is incorrect!");
+    error.status = 422;
+    throw error;
+  }
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    const error = new Error("No File uploaded!");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post!");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post Updated!", post: result });
+    })
+
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post!");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      //check loggedin user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log("result");
+      res.status(200).json({ message: "Post Deleted!" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
