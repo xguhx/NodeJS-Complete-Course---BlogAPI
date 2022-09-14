c542ca78bacc41cee861f2783479d21526691e32
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-09-14 17:00:01 -0400

    ADding Fetch Posts and pagination with GraphQl

--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -133,4 +133,38 @@ module.exports = {
       updatedAt: createdPost.updatedAt.toISOString(),
     };
   },
+
+  posts: async ({ page }, req) => {
+    if (!req.isAuth) {
+      const error = new Error("Not Authenticated!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (!page) {
+      page = 1;
+    }
+
+    const perPage = 2;
+
+    const totalPosts = await Post.find().countDocuments();
+
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate("creator");
+
+    return {
+      posts: posts.map((post) => {
+        return {
+          ...post._doc,
+          _id: post._id.toString(),
+          createdAt: post.createdAt.toISOString(),
+          updatedAt: post.updatedAt.toISOString(),
+        };
+      }),
+      totalPosts: totalPosts,
+    };
+  },
 };
